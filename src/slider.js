98f8f913113b7b758b95881327868840efe506ad
Dean Sofer <proloser@hotmail.com>
2013-06-29 19:30:00 -0700

    Corrected and updated slider to proper practices

--- src/slider.js
+++ src/slider.js
@@ -1,72 +1,59 @@
 /*
  jQuery UI Slider plugin wrapper
 */
-angular.module('ui.slider', []).value('uiSliderConfig',{}).directive('uiSlider', ['uiSliderConfig', function(uiSliderConfig) {
+angular.module('ui.slider', []).value('uiSliderConfig',{}).directive('uiSlider', ['uiSliderConfig', '$timeout', function(uiSliderConfig, $timeout) {
 	uiSliderConfig = uiSliderConfig || {};
         return {
-            restrict: 'A',
             require: 'ngModel',            
-            scope: {
-                value: '=ngModel'
-            },
-            link: function (scope, elm, $attrs, uiEvent) {
-                var expression = {};                
-                // Set attribute from element
-                if (!angular.isUndefined($attrs.min)) {
-                        expression['min'] = parseInt($attrs.min);
-                }
-                if (!angular.isUndefined($attrs.max)) {
-                        expression['max'] = parseInt($attrs.max);
-                }
-                if (!angular.isUndefined($attrs.step)) {
-                        expression['step'] = parseInt($attrs.step);
-                }
-                if (!angular.isUndefined($attrs.initValue)) {
-                    expression['value'] = parseInt($attrs.initValue);
-                   	scope.value = expression['value'];
-                }
-                if (!angular.isUndefined($attrs.range)) {
-                        expression['range'] = $attrs.range;
-                }                  
-                // Default values                      
-                var options = {
-                	range:	'min',
-                    value: scope.value,
-                    min: 1,
-                    max: 100,
-                    // Apply values
-                    slide: function (event, ui) {
-                        scope.$apply(function () {
-                            scope.value = ui.value;
-                        });
-                    },
-                    // Set initial value when slider handle is clicked
-                    start: function( event, ui ) {
-                        if (!scope.value) {
-                            scope.$apply(function () {
-                                if (!angular.isUndefined(expression['min'])) {
-                                    scope.value = expression['min'];
-                                }
-                                else if (!angular.isUndefined(uiSliderConfig['min'])) {
-                                    scope.value = parseInt(uiSliderConfig['min']);
-                                }
-                                else if (!angular.isUndefined(options['min'])) {
-                                    scope.value = parseInt(options['min']);
-                                }
-                            });
-                         }
-                    }
+            link: function ($scope, elm, $attrs, ngModel) {
+                var options = angular.extend($scope.$eval($attrs.uiSlider) || {}, uiSliderConfig);
+
+                var init = function() {
+                    elm.slider(options);
+                    init = angular.noop;
                 };
-                
-                // Watch for changes in value, update all sliders bind to the same model within scope
-                scope.$watch('value', function (newVal, oldVal) {
-                    if (!angular.isUndefined(newVal) && newVal != oldVal) {
-                        elm.slider('value', newVal);
-                    }
+
+                // convenience properties
+                var properties = ['min', 'max', 'step'];
+                $.each(properties, function(i, property){
+                    // support {{}} and watch for updates
+                    $attrs.$observe(property, function(newVal){
+                        init();
+                        elm.slider('option', property, parseInt(newVal));
+                    });
                 });
-                //Set the options from the directive's configuration
-                angular.extend(options, uiSliderConfig, expression);
-                elm.slider(options);
+                $attrs.$observe('disabled', function(newVal){
+                    init();
+                    elm.slider('option', 'disabled', !!newVal);
+                });
+
+                // Watch ui-slider (byVal) for changes and update
+                $scope.$watch($attrs.uiSlider, function(newVal){
+                    init();
+                    elm.slider('option', newVal);
+                }, true);
+                
+                // Late-bind to prevent compiler clobbering
+                $timeout(init, 0, true);
+                
+                // Update model value from slider
+                elm.bind('slide', function(event, ui){
+                    ngModel.$setViewValue(ui.values || ui.value);
+                    $scope.$apply();
+                });
+                
+                // Update slider from model value
+                ngModel.$render = function(){
+                    init();
+                    var method = options.range === true ? 'values' : 'value';
+                    elm.slider(method, ngModel.$viewValue);
+                };
+
+                function destroy(){
+                    elm.slider('destroy');
+                }
+                elm.bind('$destroy', destroy);
+                $scope.$on('$destroy', destroy);
             }
         };
     }]);
