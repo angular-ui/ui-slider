acae06f7cb4265e8d9c063f35fff724f9037844c
Douglas Duteil <douglasduteil@gmail.com>
2013-11-27 16:38:20 +0100

    chore(slider): add default style

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     files: [
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'src/ui-slider.js',
+      'src/*',
       'test/*.spec.js'
     ],
 
